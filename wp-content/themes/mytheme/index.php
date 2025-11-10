ab7487ac629f8b2bf84851c7dabae648031f4b59
unpaidcoffee <unpaidcoffee@gmail.com>
2025-11-10 13:54:32 +0200

    new layout for blog front page.

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -2,25 +2,51 @@
 get_header();
 ?>
 
-<?php
-if (have_posts () ) : while (have_posts() ) : the_post(); ?>
-    <div class="container">
-        <div class="row">
-            <div class="post-style">
-                <a href="<?php the_permalink(); ?>">
-                    <?php if(has_post_thumbnail()) {
-                        the_post_thumbnail('large');
-                    } ?>
-                    <div class="post-content">
-                        <h2><?php the_title(); ?></h2>
-                        <p><?php the_excerpt(); ?></p>
-                        <p class="date-posted">Posted: <?php echo post_time();?></p>
-                    </div>
-                </div>
+<?php if ( have_posts() ) : ?>
+  <div class="container py-5">
+    <div class="row g-4">
+      <?php 
+        $post_count = 0;
+        while ( have_posts() ) : the_post(); 
+          $post_count++;
+      ?>
+
+        <?php if ( $post_count === 1 ) : ?>
+          <!-- 🎯 First (latest) post, full width -->
+          <div class="post-style col-12">
+            <a href="<?php the_permalink(); ?>">
+              <?php if ( has_post_thumbnail() ) : ?>
+                <?php the_post_thumbnail( 'large', array( 'class' => 'img-fluid' ) ); ?>
+              <?php endif; ?>
+              <div class="post-content">
+                <h2><?php the_title(); ?></h2>
+                <p><?php the_excerpt(); ?></p>
+                <p class="date-posted">Posted: <?php echo post_time(); ?></p>
+              </div>
             </a>
-        </div>
-     </div>
-     <?php endwhile; endif; ?>
+          </div>
+
+        <?php else : ?>
+          <!-- 🧱 All other posts, 3 per row -->
+          <div class="post-style col-12 col-md-3">
+            <a href="<?php the_permalink(); ?>">
+              <?php if ( has_post_thumbnail() ) : ?>
+                <?php the_post_thumbnail( 'medium_large', array( 'class' => 'img-fluid' ) ); ?>
+              <?php endif; ?>
+              <div class="post-content">
+                <h2><?php the_title(); ?></h2>
+                <p><?php the_excerpt(); ?></p>
+                <p class="date-posted">Posted: <?php echo post_time(); ?></p>
+              </div>
+            </a>
+          </div>
+        <?php endif; ?>
+
+      <?php endwhile; ?>
+    </div>
+  </div>
+<?php endif; ?>
+
 <?php 
 get_footer();
 ?>

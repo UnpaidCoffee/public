a927ee47dca65e384fa27246ec54eb1dfe187a77
unpaidcoffee <unpaidcoffee@gmail.com>
2025-11-10 17:18:53 +0200

    article author info to blog posts

--- wp-content/themes/mytheme/page.php
+++ wp-content/themes/mytheme/page.php
@@ -1,21 +1,25 @@
-<?php 
-get_header();
-while(have_posts()) {
-    the_post(); ?>
-    <div class="page-header">
-        <?php if (has_post_thumbnail()) { ?>
-            <div class="featured-image">
-                <?php the_post_thumbnail('large'); ?>
-                <h1><?php the_title(); ?></h1>
-            </div>
-        <?php } else { ?>
-            <h1><?php the_title(); ?></h1>
-        <?php } ?>
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+  <div class="page-header">
+    <?php if ( has_post_thumbnail() ) : ?>
+      <div class="featured-image">
+        <?php the_post_thumbnail( 'large' ); ?>
+        <h1><?php the_title(); ?></h1>
+      </div>
+    <?php else : ?>
+      <h1><?php the_title(); ?></h1>
+    <?php endif; ?>
+  </div>
+
+  <div class="page-content">
+    <?php the_content(); ?>
+    <div class="post-content">
+      <h2><?php the_title(); ?></h2>
     </div>
-        <div class="page-content">
-            <?php the_content(); ?>
-        </div>
-    </div>
-<?php } 
-get_footer();
-?>
+  </div>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
--- wp-content/themes/mytheme/single.php
+++ wp-content/themes/mytheme/single.php
@@ -1,24 +1,30 @@
 <? get_header(); ?>
+
 <?php if(have_posts() ) : while(have_posts() ) : the_post(); ?>
+    <?php if ( has_post_thumbnail() ) : ?>
+    <figure class="blog-thumbnail">
+        <?php the_post_thumbnail( 'large', array( 'class' => 'img-fluid' ) ); ?>
+
+        <?php if ( get_the_post_thumbnail_caption() ) : ?>
+        <figcaption class="blog-caption">
+            <?php echo get_the_post_thumbnail_caption(); ?>
+        </figcaption>
+        <?php endif; ?>
+    </figure>
+    <?php endif; ?>
+
     <div class="container">
         <div class="row">
-            <?php if ( has_post_thumbnail() ) : ?>
-            <figure class="blog-thumbnail">
-                <?php the_post_thumbnail( 'large', array( 'class' => 'img-fluid' ) ); ?>
-
-                <?php if ( get_the_post_thumbnail_caption() ) : ?>
-                <figcaption class="blog-caption">
-                    <?php echo get_the_post_thumbnail_caption(); ?>
-                </figcaption>
-                <?php endif; ?>
-            </figure>
-            <?php endif; ?>
-
             <div class="blog-text">
-                <h2><?php the_title(); ?></h2>
-                <p><?php the_content(); ?></p>
+                <div class="author-info">
+                    <?php echo get_avatar( get_the_author_meta( 'ID' ), 48 ); ?>
+                    <p class="written-by">Written by: <?php the_author(); ?></p>
+                    <p class="date-posted">On: <?php echo post_time(); ?></p>
+                </div>
+                    <h2><?php the_title(); ?></h2>
+                    <p><?php the_content(); ?></p>
+                </div>
             </div>
         </div>
-    </div>
-<?php endwhile; endif; ?>
+    <?php endwhile; endif; ?>
 <? get_footer(); ?>

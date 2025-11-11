2efc7cad8c6a0c58c1e9c7d4095bbd197eac3a0c
unpaidcoffee <unpaidcoffee@gmail.com>
2025-11-11 11:19:24 +0200

    added a hero image feature to blog texts.

--- wp-content/themes/mytheme/single.php
+++ wp-content/themes/mytheme/single.php
@@ -1,30 +1,39 @@
-<? get_header(); ?>
+<?php get_header(); ?>
 
-<?php if(have_posts() ) : while(have_posts() ) : the_post(); ?>
-    <?php if ( has_post_thumbnail() ) : ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+  <?php if ( has_post_thumbnail() ) : ?>
     <figure class="blog-thumbnail">
-        <?php the_post_thumbnail( 'large', array( 'class' => 'img-fluid' ) ); ?>
+      <?php the_post_thumbnail( 'large', array( 'class' => 'img-fluid' ) ); ?>
 
-        <?php if ( get_the_post_thumbnail_caption() ) : ?>
+      <figcaption class="blog-title-overlay">
+        <h1 class="blog-title"><?php the_title(); ?></h1>
+      </figcaption>
+
+      <?php if ( get_the_post_thumbnail_caption() ) : ?>
         <figcaption class="blog-caption">
-            <?php echo get_the_post_thumbnail_caption(); ?>
+          <?php echo get_the_post_thumbnail_caption(); ?>
         </figcaption>
-        <?php endif; ?>
+      <?php endif; ?>
     </figure>
-    <?php endif; ?>
+  <?php endif; ?>
 
-    <div class="container">
-        <div class="row">
-            <div class="blog-text">
-                <div class="author-info">
-                    <?php echo get_avatar( get_the_author_meta( 'ID' ), 48 ); ?>
-                    <p class="written-by">Written by: <?php the_author(); ?></p>
-                    <p class="date-posted">On: <?php echo post_time(); ?></p>
-                </div>
-                    <h2><?php the_title(); ?></h2>
-                    <p><?php the_content(); ?></p>
-                </div>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="blog-text">
+
+        <p><?php the_content(); ?></p>
+
+          <div class="author-info">
+            <?php echo get_avatar( get_the_author_meta( 'ID' ), 48 ); ?>
+            <p class="written-by">Written by: <?php the_author(); ?></p>
+            <p class="date-posted">On: <?php echo post_time(); ?></p>
         </div>
-    <?php endwhile; endif; ?>
-<? get_footer(); ?>
+
+      </div>
+    </div>
+  </div>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
